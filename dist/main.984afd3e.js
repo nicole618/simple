0d73e3dc20eea4b30253b0953383d563b193988d
nicole <1251501326@qq.com>
2021-02-12 09:08:30 +0800

    解决移动端弹性回滚

--- dist/main.984afd3e.js
+++ dist/main.984afd3e.js
@@ -226,5 +226,13 @@ var randomColor = function randomColor() {
 
   return "#" + color;
 };
+
+if ('ontouchstart' in document.documentElement) {
+  document.body.addEventListener('touchmove', function (e) {
+    e.preventDefault(); //阻止默认的处理方式(阻止下拉滑动的效果)
+  }, {
+    passive: false
+  });
+}
 },{}]},{},["epB2"], null)
-//# sourceMappingURL=main.3e97a5dc.js.map
+//# sourceMappingURL=main.984afd3e.js.map
--- src/main.js
+++ src/main.js
@@ -95,3 +95,8 @@ const randomColor = () => {
     }
     return "#" + color;
 }
+if ('ontouchstart' in document.documentElement) {
+    document.body.addEventListener('touchmove', function (e) {
+        e.preventDefault(); //阻止默认的处理方式(阻止下拉滑动的效果)
+    }, { passive: false });
+}
